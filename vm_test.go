d0ec2fb1364d68344b064da19684a2372a13438f
mortdeus <mortdeus@gmail.com>
2014-11-25 13:36:11 -0600

    fleshed out more testing cases

--- vm_test.go
+++ vm_test.go
@@ -2,6 +2,7 @@ package chaos_test
 
 import (
 	. "github.com/mortdeus/chaos"
+	// "reflect"
 	"testing"
 )
 
@@ -11,35 +12,36 @@ var InstTests = map[string][]struct {
 	errCode    int
 }{
 	"add": {
-		{1, 2, new(int), -1},
-		{1.5, 2.3, new(interface{}), -1},
-		{"foo", "bar", new(interface{}), -1},
-		{"baz", "qul", new(string), -1},
-		{complex(5.10, 1.532), 5321i, new(interface{}), -1},
-		{uint32(512), uint32(1024), new(uint32), -1},
+		{1, 2, int(0), -1},
+		{1.5, 2.3, interface{}(float64(0)), -1},
+		{"foo", "bar", interface{}(""), -1},
+		{"baz", "qul", "", -1},
+		{complex(5.10, 1.532), 5321i, interface{}(complex(1.0, .5)), -1},
+		{uint32(512), uint32(1024), uint32(0), -1},
 
-		{uintptr(128), []byte("hi"), new(interface{}), TypeConv},
-		{interface{}(1), interface{}("hi"), new(interface{}), TypeConv},
+		{uintptr(128), []byte("hi"), interface{}(nil), TypeConv},
+		{interface{}(1), interface{}("hi"), interface{}(nil), TypeConv},
 
-		{interface{}("dooble"), interface{}("dobble"), new(interface{}), InvalidOp},
+		{interface{}("dooble"), interface{}("dobble"), interface{}(""), InvalidOp},
 		{true, false, new(bool), InvalidOp},
-		{func() { var i = 0; _ = i }, func() { var j = 1; _ = j }, new(func()), InvalidOp},
-		{true, false, new(bool), InvalidOp},
-		{[]int{1, 2, 3}, []int{4, 5, 6}, new([]int), InvalidOp},
-		{map[string]int{"a": 1, "b": 2}, map[string]int{"c": 3, "d": 4}, new(map[string]int), InvalidOp},
+		{func() { var i = 0; _ = i }, func() { var j = 1; _ = j }, func() {}, InvalidOp},
+		{[]int{1, 2, 3}, []int{4, 5, 6}, []int{0}, InvalidOp},
+		{map[string]int{"a": 1, "b": 2}, map[string]int{"c": 3, "d": 4}, map[string]int{"": 0}, InvalidOp},
 	},
 }
 
 func TestAdd(t *testing.T) {
 	for _, args := range InstTests["add"] {
-		err := Add(args.src1, args.src2, args.dest)
+		err := Process(Add, args.src1, args.src2, &args.dest)
 		if err != nil {
 			if e, ok := err.(TypeErr); ok {
 				if e.Code() == args.errCode {
+					//t.Log(e)
 					continue
 				}
 			}
 			t.Error(err)
 		}
+		t.Logf("%#v + %#v = %#v\n", args.src1, args.src2, args.dest)
 	}
 }
